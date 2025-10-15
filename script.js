6902db69218cf55c70e744888690ee7bc123313f
isi-code <irvinjose.silva@gmail.com>
2025-10-15 10:23:02 -0700

    `getHumanChoice function` added
    This method asks for input (rock,paper,scissors) and selects an option based on the input.

--- script.js
+++ script.js
@@ -10,9 +10,26 @@ function getComputerChoice(){
     // Get random number based on option length
     let randomNum = Math.floor(Math.random() * optionsIdx.length);
     let choice = optionsIdx[randomNum];
-    
+
     return options[choice]
 }
 
+function getHumanChoice(){
+    const choice = prompt("Choose Rock, Paper, or Scissors: ").toLowerCase();
+    try{
+        switch(choice){
+            case "rock":
+                return options.rock
+            case "paper":
+                return options.paper
+            case "scissors":
+                return options.scissors
+            default:
+                throw new Error(`${choice} is an invalid input`);
+        }
+    } catch (error) {
+        console.error(error.message);
+    }
+}
 
 
