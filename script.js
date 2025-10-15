ef5d8d8e0d001ff4a8c8193272934f49a3d08bba
isi-code <irvinjose.silva@gmail.com>
2025-10-15 10:13:57 -0700

    Add getComputerChoice function and initialize game variables
    - Added an object to store player and computer options.
    - Created variables to track player and computer scores.
    - Implemented getComputerChoice to return a random option key from the choices object.

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-console.log("Hello World");
+const humanScore = 0 
+const computerScore = 0
 
-const choices = {"rock":'🪨', "paper":'📃', "scissors" :'✂️'}
+const options = {"rock":'🪨', "paper":'📃', "scissors" :'✂️'}
+const stopPlaying = false
+
+function getComputerChoice(){
+    // Get option object keys as an array
+    const optionsIdx = Object.keys(options);
+    // Get random number based on option length
+    let randomNum = Math.floor(Math.random() * optionsIdx.length);
+    let choice = optionsIdx[randomNum];
+    
+    return options[choice]
+}
+
+
+
